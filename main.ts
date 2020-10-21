1febd0ade07b78be8aae2ce224b42e8b1d6786bb
Andrew <afmcdnl@gmail.com>
2020-10-21 08:29:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 basic.showString("Compass Sender")
 radio.setGroup(1)
+// In this code we just need to send the value of the compass heading to the other micro:bit and it can do the direction determination
 basic.forever(function () {
     radio.sendNumber(input.compassHeading())
     basic.pause(500)
